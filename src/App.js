5dbcc9b28090fefaa792ee62244b0b0046739505
elenaBushueva <helenabushueva@gmail.com>
2021-11-09 20:50:25 -0800

    dima-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [flag, setFlag] = useState(true);
+
+    let shape;
+
+    if (flag === true) {
+        shape = "square"
+    } else {
+        shape = "circle"
+    }
+
+    return (
+        <div>
+            <div className={shape} onClick={() => setFlag(!flag)}>
+            </div>
+        </div>
+    );
 }
 
 export default App;
